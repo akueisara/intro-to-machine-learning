287b3dd96e14e0c9ef205b56e5299b8491ee85b0
Kuei-Jung Hu <akueisara39@gmail.com>
2016-10-23 20:11:39 -0700

    finish svm

--- ud120-projects/svm/svm_author_id.py
+++ ud120-projects/svm/svm_author_id.py
@@ -24,7 +24,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf", C=10000.)
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+from sklearn.metrics import accuracy_score
+print "SVM accuracy: %r" % accuracy_score(pred, labels_test)
+print "10th: %r, 26th: %r, 50th: %r" % (pred[10], pred[26], pred[50])
+print "No. of predicted to be in the 'Chris'(1): %r" % sum(pred)
 #########################################################
 
 
