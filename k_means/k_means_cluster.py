2ddc2fbb1b441736f50a6479f3bfc051c80fe38f
Kuei-Jung Hu <akueisara39@gmail.com>
2017-01-11 11:08:42 -0800

    Finish k_means

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,7 +48,9 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
+# features_list = [poi, feature_1, feature_2, feature_3]
 features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
@@ -58,14 +60,17 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+# for f1, f2, f3 in finance_features:
+for f1, f2 in finance_features:   
+    plt.scatter( f1, f2)
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
-
+from sklearn.cluster import KMeans
+clf = KMeans(n_clusters=2)
+pred = clf.fit_predict(finance_features)
+Draw(pred, finance_features, poi, name="clusters_before_scaling.pdf", f1_name=feature_1, f2_name=feature_2)
 
 
 ### rename the "name" parameter when you change the number of features
@@ -74,3 +79,23 @@ try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
+
+
+stock = []
+for i in data_dict:
+    value = data_dict[i]["exercised_stock_options"]
+    if value == 'NaN':
+        continue
+    stock.append(value)
+print max(stock)
+print min(stock)
+
+salary = []
+for i in data_dict:
+    value = data_dict[i]["salary"]
+    if value == 'NaN':
+        continue
+    salary.append(value)
+    
+print max(salary)
+print min(salary)
